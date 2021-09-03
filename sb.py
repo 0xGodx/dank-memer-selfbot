252f9d066153561bad2be22afd24605f5e1d7e73
God <85898288+0xGodx@users.noreply.github.com>
2021-09-03 21:55:21 +0100

    Update sb.py

--- sb.py
+++ sb.py
@@ -1,31 +1,28 @@
-import discord, random, asyncio
-from discord.ext import commands
+import random, requests, time
 
-client = commands.Bot(command_prefix=(">>>"), self_bot=True)
 
-@client.event
-async def on_ready():
-    print(f'''We have logged in as {client.user}\nLogged in {len(client.guilds)} servers''')
-
-@client.command()
-async def dank(ctx):
+class dank():
     while True:
-        randomlist = []
-        for i in range(0,5):
-                #   Random await speed
-            n = random.randint(40,60)
-            randomlist.append(n)
-            print(f"Delay is on {n}")
+            randomlist = []
+            for i in range(0,5):
+                    #   Random await speed
+                n = random.randint(40,60)
+                randomlist.append(n)
+                print(f"Delay is on {n}")
 
-                #   Discord stuff
-            discordchannelid = discord.utils.get(ctx.guild.channels, id=ya channel id)
-            await discordchannelid.send("pls hunt")
-            await asyncio.sleep(5)
-            await discordchannelid.send("pls fish")
-            await asyncio.sleep(5)
-            await discordchannelid.send("pls dig")
+                    #   Discord stuff
+                url = "https://discord.com/api/v9/channels/<SET YOUR DISCORD CHANNEL ID HERE>/messages"
+               
+                headers = {"Authorization" : "REPLACE WITH YOUR DISCORD TOKEN"}
 
-                #   Await 
-            await asyncio.sleep(n)
+                    #   Request stuff
 
-client.run('ya token', bot = False)
+                requests.post(url, headers=headers, data={"content" : "pls hunt"})
+                
+                time.sleep(5)
+                requests.post(url, headers=headers, data={"content" : "pls fish"})
+                time.sleep(5)
+                requests.post(url, headers=headers, data={"content" : "pls dig"})
+
+                    #   Await 
+                time.sleep(n)
